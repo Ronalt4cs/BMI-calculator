280fe4346674a6a01caadac3921106df80e4f677
ronalt augusto <ronaltaugustocs@gmmail.com>
2023-01-07 16:10:55 -0300

    functions added

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# BMI calculator | calculadora de IMC
+
+Simple BMI calculator
+
+## Demonstração
+
+![preview](assets/preview.png)
+
+## 🔗 Links
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/ronalt-augusto-66202b24b/)
--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-// how to remove border input with css?      
+const inputWeight = document.getElementById('input-weight');
+const inputHeight = document.getElementById('input-height');
+const showResult = document.querySelector('.show-bmi');
+const btnCalculate = document.querySelector('.btn-calculate');
+const btnClear = document.querySelector('.btn-clear');
+const spanError = document.querySelector('.span-error');
+const spanResult = document.querySelector('.span-result');
+
+const calculateBmi = () => {
+   if (!inputHeight.value || !inputWeight.value) {
+      spanError.textContent = 'Preencha o peso e a altura';
+      spanResult.innerHTML = ''
+      return
+   }
+   let weightValue = inputWeight.value;
+   let heightValue = inputHeight.value;
+
+   const bmiValue = weightValue / (heightValue * heightValue);
+   spanResult.textContent = `Seu imc é: ${bmiValue.toFixed(2)}`;
+}
+
+btnCalculate.addEventListener('click', () => {
+   calculateBmi();
+});
+
+btnClear.addEventListener('click', () => {
+   inputWeight.value = '';
+   inputHeight.value = '';
+   spanResult.textContent = 'Seu IMC';
+});
